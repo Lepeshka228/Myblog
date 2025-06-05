18cd9cdae0a41d05d21d29bded6428e95bd6aab6
Alexandr <romanovsasa26@gmail.com>
2025-06-05 03:08:57 +0300

    переместил migrations в instance

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'the_most_secret_key_you_have_ever_seen'
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')      # местоположение БД
+        'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')      # местоположение БД
     SQLALCHEMY_TRACK_MODIFICATIONS = False      # отключение функции оповещения приложения об изменениях БД
 
     #данные сервера эл почты (для отправки ошибок)
